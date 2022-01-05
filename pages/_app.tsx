e45d22efc80beeac133f0b9a8183f7b0a47d3ffb
capow20 <cpowell25@uco.edu>
2022-01-04 19:06:46 -0600

    saving

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import createMixins from '@mui/material/styles/createMixins'
+import { useStore } from '../stores/store';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const {userStore} = useStore();
 
+  useEffect(() => {
+    userStore.getTokenFromLocalStorage();
+  },[])
+  
   return <Component {...pageProps} />
 }
 
--- stores/userStore.ts
+++ stores/userStore.ts
@@ -1,14 +1,27 @@
-import { makeAutoObservable, runInAction } from "mobx";
+import { makeAutoObservable, reaction, runInAction } from "mobx";
 import agent from "../axios/agent";
 import { LoginDto } from "../Dtos/LoginDto";
 import { AppUser } from "../models/appUser";
 
 export default class UserStore{
     isRegistering: boolean = false;
-    currentUser: AppUser | undefined = undefined; 
+    currentUser: AppUser | undefined = undefined;
+    token: string | null = window.localStorage.getItem('token'); 
 
     constructor(){
         makeAutoObservable(this);
+
+        reaction(
+            () => this.token,
+            token => {
+                if(token){
+                    window.localStorage.setItem("token", token);
+                }else{
+                    window.localStorage.removeItem('token');
+                }
+                
+            }
+        )
     }
 
     setIsRegistering = (state: boolean) => {
@@ -17,13 +30,39 @@ export default class UserStore{
         })
     }
 
+    setToken = (token: string) => {
+        runInAction(() => {
+            this.token = token;
+        })
+    }
+
+    getTokenFromLocalStorage = () => {
+        let token: string | null = localStorage.getItem("token");
+        if(!token) return;
+        runInAction(() => {
+            this.token = token;
+        })
+    }
+
     login = async (creds: LoginDto) => {
         await agent.Account.login(creds).then((res: AppUser) => {
             runInAction(() => {
                 this.currentUser = res;
+                this.token = res.token;
             })
         }).catch((error) => {throw error})
 
         console.log("Current user is: ", this.currentUser);
+    }
+
+    getUser = async () => {
+        await agent.Account.current().then((res) => {
+            runInAction(() => {
+                this.currentUser = res;
+            })
+        }).catch((e) => {
+            throw e;
+        })
+        
     }
 }
